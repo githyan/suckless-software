6deb9a6f8029319673bfe9a05492f7c4dd215fa4
githyan <nantadryanek4@gmail.com>
2024-10-15 03:30:41 +0700

    first commit

---
+++ themes/gruvbox.h
@@ -0,0 +1,7 @@
+static const char *fonts[] = { "FiraCode Nerd Font:style=Medium:size=10" };
+static const char dmenufont[] = { "FiraCode Nerd Font:style=Medium:size10"};
+static const char col_gray1[] = "#222222";
+static const char col_gray2[] = "#444444";
+static const char col_gray3[] = "#bbbbbb";
+static const char col_gray4[] = "#eeeeee";
+static const char col_cyan[] = "#d79921";
