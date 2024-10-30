6ab295bad170eaebbd77210c9f8147beeaac0ecf
githyan <nantadryanek4@gmail.com>
2024-10-30 14:50:08 +0700

    fixes anybar and ipc patch

--- dwms/config.h
+++ dwms/config.h
@@ -10,6 +10,10 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
+static const char *altbarclass      = "Polybar";
+static const char *alttrayname      = "tray";
+static const char *altbarcmd        = "$HOME/.config/polybar/blocks/launch.sh";
 static const int horizpadbar        = 2;
 static const int vertpadbar         = 1;
 static const int vertpad            = 8;
@@ -17,19 +21,19 @@ static const int sidepad            = 5;
 #define ICONSIZE 20   /* icon size */
 #define ICONSPACING 5 /* space between icon and title */
 #include "themes/gruvbox.h"
-
+#include "/home/archaen/.cache/wal/colors-wal-dwm.h"
 static unsigned int baralpha        = 0xb0;
 static unsigned int borderalpha     = OPAQUE;
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeStatus]  = { col_gray3, col_gray1,  col_gray3  }, // Statusbar right {text,background,not used but cannot be empty}
-	[SchemeTagSel]  = { col_gray4, col_cyan,  col_gray2  }, // Tagbar left selected {text,background,not used but cannot be empty}
-	[SchemeTagsNorm]  = { col_gray3, col_gray1,  col_cyan  }, // Tagbar left unselected {text,background,not used but cannot be empty}
-	[SchemeInfoSel]  = { col_gray4, col_cyan,  col_gray1  }, // infobar middle  selected {text,background,not used but cannot be empty}
-	[SchemeInfoNorm]  = { col_gray3, col_gray1,  col_gray4 }, // infobar middle  unselected {text,background,not used but cannot be empty}
-};
+// static const char *colors[][3]      = {
+// 	/*               fg         bg         border   */
+// 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+// 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+// 	[SchemeStatus]  = { col_gray3, col_gray1,  col_gray3  }, // Statusbar right {text,background,not used but cannot be empty}
+// 	[SchemeTagSel]  = { col_gray4, col_cyan,  col_gray2  }, // Tagbar left selected {text,background,not used but cannot be empty}
+// 	[SchemeTagsNorm]  = { col_gray3, col_gray1,  col_cyan  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+// 	[SchemeInfoSel]  = { col_gray4, col_cyan,  col_gray1  }, // infobar middle  selected {text,background,not used but cannot be empty}
+// 	[SchemeInfoNorm]  = { col_gray3, col_gray1,  col_gray4 }, // infobar middle  unselected {text,background,not used but cannot be empty}
+// };
 
 /* tagging */
 static const char *tags[] = {"一", "二", "三","四","五","六","七","八","九"};
@@ -202,4 +206,22 @@ static const Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
+
+static const char *ipcsockpath = "/tmp/dwm.sock";
+static IPCCommand ipccommands[] = {
+  IPCCOMMAND( view,                  1,              {ARG_TYPE_UINT}  ),
+  IPCCOMMAND( toggleview,            1,              {ARG_TYPE_UINT}  ),
+  IPCCOMMAND( tag,                   1,              {ARG_TYPE_UINT}  ),
+  IPCCOMMAND( toggletag,             1,              {ARG_TYPE_UINT}  ),
+  IPCCOMMAND( tagmon,                1,              {ARG_TYPE_UINT}  ),
+  IPCCOMMAND( focusmon,              1,              {ARG_TYPE_SINT}  ),
+  IPCCOMMAND( focusstack,            1,              {ARG_TYPE_SINT}  ),
+  IPCCOMMAND( zoom,                  1,              {ARG_TYPE_NONE}  ),
+  IPCCOMMAND( incnmaster,            1,              {ARG_TYPE_SINT}  ),
+  IPCCOMMAND( killclient,            1,              {ARG_TYPE_SINT}  ),
+  IPCCOMMAND( togglefloating,        1,              {ARG_TYPE_NONE}  ),
+  IPCCOMMAND( setmfact,              1,              {ARG_TYPE_FLOAT} ),
+  IPCCOMMAND( setlayoutsafe,         1,              {ARG_TYPE_PTR}   ),
+  IPCCOMMAND( quit,                  1,              {ARG_TYPE_NONE}  ),
 };
