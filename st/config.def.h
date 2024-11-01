ea123c0b79e75c501f82389870fefb43ea7f6184
githyan <nantadryanek4@gmail.com>
2024-11-01 14:41:37 +0700

    Modified st config

--- st/config.def.h
+++ st/config.def.h
@@ -97,62 +97,7 @@ unsigned int tabspaces = 8;
 float alpha = 0.85; 
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	[0] = "#282828",	 /* hard constant: #1d2021 / soft contrast: #32302f */
-	[1] = "#cc241d",	 /* red */
-	[2] = "#98971a",	 /* green */
-	[3] = "#d79921",	 /* yellow */
-	[4] = "#458588",	 /* blue */
-	[5] = "#b16286",	 /* magenta */
-	[6] = "#689d6a",	 /* cyan */
-	[7] = "#a89984",	 /* white */
-	[8] = "#928374",	 /* black */
-	[9] = "#fb4934",	 /* red */
-	[10] = "#b8bb26",	 /* green */
-	[11] = "#fabd2f",	 /* yellow */
-	[12] = "#83a598",	 /* blue */
-	[13] = "#d3869b",	 /* magenta */
-	[14] = "#8ec076",	 /* cyan */
-	[15] = "#ebdbb2", 	 /* white */ 
-/*  "#ff5733",
-    "#ffda33",
-    "#33ff57",
-    "#338aff",
-    "#ff33f5",
-    "#33e6ff",
-    "#ff5733",
-    "#ff8533",
-    "#ffcf33",
-    "#33ff6b",
-    "#33a6ff",
-    "#ff33b5",
-    "#33f7ff",
-    "#ff5733",
-    "#ffa833",
-    "#ffd933",
-
-    [255] = 0,
-
-    "#ffda33",
-    "#ff5733",
-    "#000000",
-    "#ffffff",
-*/
-
-};
-/*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-//unsigned int defaultfg = 259;
-//unsigned int defaultbg = 258;
-//unsigned int defaultcs = 256;
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 0;
-unsigned int defaultcs = 15;
-
-static unsigned int defaultrcs = 257;
-
+#include "/home/archaen/.cache/wal/colors-wal-st.h"
 /*
  * Default shape of cursor
  * 2: Block ("█")
