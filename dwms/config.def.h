8e86fae252e5ced3e16ff974cfc2803791e94ccf
githyan <nantadryanek4@gmail.com>
2024-11-30 22:32:02 +0700

    change fonts

--- dwms/config.def.h
+++ dwms/config.def.h
@@ -1,22 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char *fonts[] = { "FiraCode Nerd Font:style=Medium:size=10" };
-static const char dmenufont[] = { "FiraCode Nerd Font:style=Medium:size10"};
+
+static const char *fonts[] = {"Fixedsys Excelsior:size=12:antialias=true:autohint=false"};
+static const char dmenufont[] = "Fixedsys Excelsior:size=12:antialias=true:autohint=false";
 
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 2;
 static const int vertpadbar         = 1;
-static const int vertpad            = 8;
-static const int sidepad            = 5;
+static const int vertpad            = 10;
+static const int sidepad            = 10;
 #include "/home/archaen/.cache/wal/colors-wal-dwm.h"
 static char termcol0[] = "#000000"; /* black   */
 static char termcol1[] = "#ff0000"; /* red     */
