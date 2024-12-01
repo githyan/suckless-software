d02f4df8a734c367850dec91a87724d9cf781a13
githyan <nantadryanek4@gmail.com>
2024-12-01 12:32:07 +0700

    small changes

--- dwms/config.def.h
+++ dwms/config.def.h
@@ -2,11 +2,11 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int snap      = 25;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -16,8 +16,8 @@ static const char *alttrayname      = "tray";
 static const char *altbarcmd        = "$HOME/.config/polybar/blocks/launch.sh";
 static const int horizpadbar        = 2;
 static const int vertpadbar         = 1;
-static const int vertpad            = 8;
-static const int sidepad            = 5;
+static const int vertpad            = 10;
+static const int sidepad            = 10;
 #define ICONSIZE 20   /* icon size */
 #define ICONSPACING 5 /* space between icon and title */
 #include "themes/colors-wal-dwm.h"
