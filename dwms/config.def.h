c61f86fdcea59f0bd0c409559315d1d13657260b
githyan <nantadryanek4@gmail.com>
2025-01-04 12:30:54 +0700

    fix stacking systray in the bugged bar

--- dwms/config.def.h
+++ dwms/config.def.h
@@ -1,22 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 
+/* autostart*/
+static const char autostartblocksh[] = "autostart_blocking.sh";
+static const char autostartsh[] = "autostart.sh";
+static const char dwmdir[] = "dwm";
+static const char localshare[] = ".local/share";
+
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int gappih = 15;  /* horiz inner gap between windows */
-static const unsigned int gappiv = 6;   /* vert inner gap between windows */
+
+static const unsigned int gappih = 5; /* horiz inner gap between windows */
+static const unsigned int gappiv = 5; /* vert inner gap between windows */
 static const unsigned int gappoh =
     5; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov =
-    5; /* vert outer gap between windows and screen edge */
+    15; /* vert outer gap between windows and screen edge */
 static int smartgaps =
     0; /* 1 means no outer gap when there is only one window */
-static const int showbar = 1; /* 0 means no bar */
-static const int topbar = 0;  /* 0 means bottom bar */
-static const int horizpadbar = 0;
-static const int vertpadbar = 0;
-static const int vertpad = 6;
-static const int sidepad = 20;
+
 static const unsigned int systraypinning =
     0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
           X */
@@ -27,26 +29,23 @@ static const int systraypinningfailfirst =
     1; /* 1: if pinning fails, display systray on the first monitor, False:
           display systray on the last monitor*/
 static const int showsystray = 1; /* 0 means no systray */
+static const int vertpad = 10;    /* vertical padding of bar */
+static const int sidepad = 60;    /* horizontal padding of bar */
+static const int showbar = 1;     /* 0 means no bar */
+static const int topbar = 0;      /* 0 means bottom bar */
+static const int user_bh =
+    2; /* 2 is the default spacing around the bar's font */
+
 static const char *fonts[] = {"monospace:size=10"};
 static const char dmenufont[] = "monospace:size=10";
-
-/* autostart */
-static const char broken[] = "broken";
-static const char autostartblocksh[] = "autostart_blocking.sh";
-static const char autostartsh[] = "autostart.sh";
-static const char dwmdir[] = "dwm";
-static const char localshare[] = ".local/share";
-
-/* cool autostart */
-static const char *const autostart[] = {
-    "st", NULL, NULL /* terminate */
-};
 
 static char normbgcolor[] = "#222222";
 static char normbordercolor[] = "#444444";
 static char normfgcolor[] = "#bbbbbb";
 static char selfgcolor[] = "#eeeeee";
 static char selbordercolor[] = "#005577";
+
+/*status2d xrdb colors*/
 static char selbgcolor[] = "#005577";
 static char termcol0[] = "#000000";  /* black   */
 static char termcol1[] = "#ff0000";  /* red     */
@@ -70,31 +69,23 @@ static char *termcolor[] = {
     termcol12, termcol13, termcol14, termcol15,
 };
 
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
 static char *colors[][3] = {
-    /*               fg         bg         border   */
+    /*               fg           bg           border   */
     [SchemeNorm] = {normfgcolor, normbgcolor, normbordercolor},
     [SchemeSel] = {selfgcolor, selbgcolor, selbordercolor},
 };
-static const unsigned int alphas[][3] = {
-    /*               fg      bg        border     */
-    [SchemeNorm] = {OPAQUE, baralpha, borderalpha},
-    [SchemeSel] = {OPAQUE, baralpha, borderalpha},
-};
-
 /* tagging */
+/*static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};*/
 static const char *tags[] = {"一", " 二", "三", "四", "五",
                              "六", "七",  "八", " 九"};
 static const unsigned int ulinepad =
-    5; /* horizontal padding between the underline and tag */
+    2; /* horizontal padding between the underline and tag */
 static const unsigned int ulinestroke =
-    2; /* thickness / height of the underline */
+    3; /* thickness / height of the underline */
 static const unsigned int ulinevoffset =
     0; /* how far above the bottom of the bar the line should appear */
 static const int ulineall =
     0; /* 1 to show underline on all tags, 0 for just the active ones */
-
 static const Rule rules[] = {
     /* xprop(1):
      *	WM_CLASS(STRING) = instance, class
@@ -109,6 +100,14 @@ static const Rule rules[] = {
     {"steam", "steamwebhelper", "Steam", 1 << 8, 1, -1},
 };
 
+static const char *incvol[] = {"/usr/bin/amixer", "set", "Master", "5%+", NULL};
+static const char *decvol[] = {"/usr/bin/amixer", "set", "Master", "5%-", NULL};
+static const char *mutevol[] = {"/usr/bin/amixer", "set", "Master", "toggle",
+                                NULL};
+static const char *scrot[] = {
+    "scrot", "/home/archaen/Pictures/screenshot/%Y-%m-%d-%T.png", NULL};
+static const char *scrot_select[] = {
+    "scrot", "-s", "/home/archaen/Pictures/screenshot/%Y-%m-%d-%T.png", NULL};
 /* layout(s) */
 static const float mfact = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;    /* number of clients in master area */
@@ -116,12 +115,11 @@ static const int resizehints =
     1; /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen =
     1; /* 1 will force focus on the fullscreen window */
-
 #define FORCE_VSPLIT                                                           \
   1 /* nrowgrid layout: force two clients to always split vertically */
-#define BAR_PADDING_PATCH 1
+#include "movestack.c"
 #include "vanitygaps.c"
-#include <X11/XF86keysym.h>
+
 static const Layout layouts[] = {
     /* symbol     arrange function */
     {"[]=", tile}, /* first entry is default */
@@ -158,30 +156,15 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {
-    "dmenu_run", "-c",  "-l",        "15",  "-fn",          dmenufont, "-nb",
-    normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, NULL};
+static const char *dmenucmd[] = {"dmenu_run",    "-fn", dmenufont,   "-nb",
+                                 normbgcolor,    "-nf", normfgcolor, "-sb",
+                                 selbordercolor, "-sf", selfgcolor,  NULL};
 static const char *termcmd[] = {"st", NULL};
 
-static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = {"st", "-t",     scratchpadname,
-                                      "-g", "120x34", NULL};
-
-static const char *incvol[] = {"/usr/bin/amixer", "set", "Master", "5%+", NULL};
-static const char *decvol[] = {"/usr/bin/amixer", "set", "Master", "5%-", NULL};
-static const char *mutevol[] = {"/usr/bin/amixer", "set", "Master", "toggle",
-                                NULL};
-static const char *scrot[] = {
-    "scrot", "/home/archaen/Pictures/screenshot/%Y-%m-%d-%T.png", NULL};
-static const char *scrot_select[] = {
-    "scrot", "-s", "/home/archaen/Pictures/screenshot/%Y-%m-%d-%T.png", NULL};
-
-#include "movestack.c"
 static const Key keys[] = {
     /* modifier                     key        function        argument */
     {MODKEY, XK_p, spawn, {.v = dmenucmd}},
     {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
-    {MODKEY, XK_grave, togglescratch, {.v = scratchpadcmd}},
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
@@ -247,8 +230,11 @@ static const Key keys[] = {
     {0, XF86XK_AudioMute, spawn, {.v = mutevol}},
     {0, XK_Print, spawn, {.v = scrot}},
     {ShiftMask, XK_Print, spawn, {.v = scrot_select}},
-};
+    {MODKEY, XK_minus, scratchpad_show, {0}},
+    {MODKEY | ShiftMask, XK_minus, scratchpad_hide, {0}},
+    {MODKEY, XK_equal, scratchpad_remove, {0}},
 
+};
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
  * ClkClientWin, or ClkRootWin */
